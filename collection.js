b820afe10d618b59aa71766d2196048e2f5514af
Joelle Everaert <72984240+Joelle-Everaert@users.noreply.github.com>
2020-11-10 16:40:38 +0100

    V8 + fichier js

---
+++ collection.js
@@ -0,0 +1,81 @@
+const collection = [
+        {
+            title: 'Friends',
+            author: ['Marta Kauffman', 'David Crane'],
+            category: ['Comedy', 'Romance'],
+            link: 'https://www.youtube.com/watch?v=-6-rrJIL-nM',
+            description: " Les péripéties de six jeunes new-yorkais liés par une profonde amitié. Entre amour, travail et famille, ils partagent leurs bonheurs et leurs soucis.",
+        },
+
+        {
+            title: 'How I met your Mother',
+            author: ['Carter Bays', 'Graig Thomas'],
+            category: ['Comedy', 'Romance'],
+            link: 'https://www.youtube.com/watch?v=tCy5IbsQi4s',
+            description: "Le Ted du futur raconte en 2030 à ses enfants comment il a rencontré leur mère en écumant les soirées hallucinantes avec ses meilleurs amis.",
+        },
+
+        {
+            title: 'How to Get Away with Murder',
+            author: 'Peter Nowalk',
+            category: ['Thriller', 'Mystery'],
+            link: 'https://www.youtube.com/watch?v=xsNolDDn7-I',
+            description: "La série raconte l’histoire d'Annalise Keating, professeure de droit pénal et avocate renommée à la tête de son propre cabinet à Philadelphie. Chaque année, quelques-uns de ses étudiants ont le privilège de travailler dans son cabinet. Mais un jour, ces derniers sont impliqués dans un meurtre…",
+        },
+
+        {
+            title: 'Orange is the new black',
+            author: 'Jenji Kohan',
+            category: ['Comedy', 'Drama'],
+            link: 'https://www.youtube.com/watch?v=lNvocVbXE_Y',
+            description: "Une new-yorkaise privilégiée finit en prison quand elle est rattrapée par son passé criminel.",
+        },
+
+        {
+            title: 'Game of Thrones',
+            author: ['David Benioff', 'D. B. Weiss', 'George R. R. Martin'],
+            category: ['Fantasy', 'Drama'],
+            link: 'https://www.youtube.com/watch?v=aAF12LNAeNI',
+            description: "Neuf familles nobles rivalisent pour le contrôle du Trône de Fer dans les sept royaumes de Westeros. Pendant ce temps, des anciennes créatures mythiques oubliées reviennent pour faire des ravages.",
+        },
+
+        {
+            title: 'Stranger Things',
+            author: ['Matt Duffer', 'Ross Duffer'],
+            category: ['Science-fiction', 'Horror'],
+            link: 'https://www.youtube.com/watch?v=IZeBDCuApTo',
+            description: "À Hawkins, dans l'Indiana, en 1983. Lorsque Will Byers disparaît de son domicile, ses amis se lancent dans une recherche semée d'embûches pour le retrouver.",
+        },
+
+        {
+            title: 'The Walking dead',
+            author: 'Frank Darabont',
+            category: ['Horror', 'Drama'],
+            link: 'https://www.youtube.com/watch?v=AbtiqJGhWyY',
+            description: "Rick Grimes cherche sa femme et son fils après s'être réveillé d'un long coma. Il découvre alors un monde désolé, ravagé par une terrible épidémie.",
+        },
+
+        {
+            title: 'Black Mirror',
+            author: 'Charlie Brooker',
+            category: ['Science-fiction', 'Thriller'],
+            link: 'https://www.youtube.com/watch?v=nSDviEdvw4U',
+            description: "haque épisode de cette anthologie montre la dépendance des hommes vis-à-vis de tout ce qui a un écran et les conséquences de cette dépendance.",
+        },
+
+        {
+            title: 'Scorpion',
+            author: 'Nick Santora',
+            category: ['Mystery', 'Drama'],
+            link: 'https://www.youtube.com/watch?v=BnIT59GUzto',
+            description: "Inspiré d'une histoire vraie, le projet est centré Walter, un génie excentrique, et son réseau international de super-génies qui forment la dernière ligne de défense contre les menaces complexes du monde moderne.",
+        },
+
+        {
+            title: 'The Umbrella Academy',
+            author: ['Jeremy Slater','Steve Blackman'],
+            category: ['Fantastic', 'Drama'],
+            link: 'https://www.youtube.com/watch?v=95mXLi_6tW8',
+            description: "Une famille de super-héros déjantés se réunit pour tenter de résoudre le mystère entourant la mort de leur père, la menace d'une apocalypse et bien plus encore. Adaptée de la bande dessinée de Gerard Way et Gabriel Bá.",
+        },
+    ]
--- liste de serie.md
+++ liste de serie.md
@@ -2,25 +2,25 @@ lise de serie
 
 <!-- 1. Friends 
     * crée par Marta Kauffman et David Crane
-    * Comédie/Sitcom
+    * Comedy Romance 
     * bande annonce :  https://www.youtube.com/watch?v=-6-rrJIL-nM
     * Les péripéties de six jeunes new-yorkais liés par une profonde amitié. Entre amour, travail et famille, ils partagent leurs bonheurs et leurs soucis.
 
 2. How I met your Mother
     * crée par Carter Bays et Craig Thomas
-    * Comedie/Sitcom
+    * Comedy Romance 
     * bande annonce : https://www.youtube.com/watch?v=tCy5IbsQi4s
     * Le Ted du futur raconte en 2030 à ses enfants comment il a rencontré leur mère en écumant les soirées hallucinantes avec ses meilleurs amis. -->
 
 <!-- 3. How to Get Away with Murder
     * crée par 	Peter Nowalk
-    * thriller et  Policier
+    * Thriller et  Mystery
     * bande annonce : https://www.youtube.com/watch?v=xsNolDDn7-I
     * La série raconte l’histoire d'Annalise Keating, professeure de droit pénal et avocate renommée à la tête de son propre cabinet à Philadelphie. Chaque année, quelques-uns de ses étudiants ont le privilège de travailler dans son cabinet. Mais un jour, ces derniers sont impliqués dans un meurtre… -->
 
 <!-- 4. Orange is the new black
     * crée par Jenji Kohan
-    * Comédie dramatique
+    * Comedy Drama
     * bande annonce : https://www.youtube.com/watch?v=lNvocVbXE_Y
     * Une new-yorkaise privilégiée finit en prison quand elle est rattrapée par son passé criminel. -->
 
@@ -28,36 +28,36 @@ lise de serie
 
 <!-- 5. Game of Thrones
     * Crée par David Benioff, D. B. Weiss et George R. R. Martin
-    * Fantasy drame
+    * Fantasy Drama
     * bande annonce: https://www.youtube.com/watch?v=aAF12LNAeNI
     * Neuf familles nobles rivalisent pour le contrôle du Trône de Fer dans les sept royaumes de Westeros. Pendant ce temps, des anciennes créatures mythiques oubliées reviennent pour faire des ravages. -->
 
 <!-- 6. Stranger Things
     * Crée par Matt Duffer et Ross Duffer
-    * Science-fiction, fantastique, Épouvante-horreur 
+    * Science-fiction, Horror
     * Bande anonce : https://www.youtube.com/watch?v=IZeBDCuApTo
     * À Hawkins, dans l'Indiana, en 1983. Lorsque Will Byers disparaît de son domicile, ses amis se lancent dans une recherche semée d'embûches pour le retrouver. -->
 
 <!-- 7. The Walking dead
     * Crée par Frank Darabont 
-    * Épouvante-horreur et drame 
+    * Horror et Drama 
     * Bande annonce : https://www.youtube.com/watch?v=AbtiqJGhWyY
     * Rick Grimes cherche sa femme et son fils après s'être réveillé d'un long coma. Il découvre alors un monde désolé, ravagé par une terrible épidémie. -->
 
 <!-- 8. Black Mirror
     * Crée par Charlie Brooker 
-    * Science-fiction   , thriller, anthologique 
+    * Science-fiction   , Thriller,
     * Bande annonce : https://www.youtube.com/watch?v=nSDviEdvw4U
     * Chaque épisode de cette anthologie montre la dépendance des hommes vis-à-vis de tout ce qui a un écran et les conséquences de cette dépendance. -->
 
 <!-- 9. Scorpion
     * Crée par Nick Santora
-    * Policier et Drame 
+    * Mystery et Drama 
     * Bande annonce : https://www.youtube.com/watch?v=BnIT59GUzto
     * Inspiré d'une histoire vraie, le projet est centré Walter, un génie excentrique, et son réseau international de super-génies qui forment la dernière ligne de défense contre les menaces complexes du monde moderne. -->
 
 <!-- 10. The Umbrella Academy
     * Crée par Jeremy Slater et Steve Blackman 
-    * Fantastique et Drame
+    * Fantastic et Drama
     * Bande annonce : https://www.youtube.com/watch?v=95mXLi_6tW8
     * Une famille de super-héros déjantés se réunit pour tenter de résoudre le mystère entourant la mort de leur père, la menace d'une apocalypse et bien plus encore. Adaptée de la bande dessinée de Gerard Way et Gabriel Bá. -->
